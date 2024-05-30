fd435f85004c209eb8235abdf87de499f2301dbc
Carlos Jorge Gonzalez Perales <carlosgzzperales@gmail.com>
2024-05-29 18:28:57 -0600

    Made and svg file to test out the response of inquirer

--- index.js
+++ index.js
@@ -1,5 +1,17 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
+const renderSvg = ({ text, textColor, shape, shapeColor }) =>
+    `<svg version="1.1"
+        width="300" height="200"
+        xmlns="http://www.w3.org/2000/svg">
+   
+     <rect width="100%" height="100%" fill="white" />
+    
+     <circle cx="150" cy="100" r="80" fill="${shapeColor}" />
+     <text x="150" y="125" font-size="60" text-anchor="middle" fill="${textColor}">${text}</text>
+   
+   </svg>
+        `;
 
 
 inquirer
@@ -52,7 +64,7 @@ inquirer
     
 ])
 .then((data) => {
-    const createLogo = generateLogo(data);
+    const createLogo = renderSvg(data)
 
     fs.writeFile('Logo.svg', createLogo, (err) =>
     err ? console.log(err) : console.log('Generated Logo.svg'));
